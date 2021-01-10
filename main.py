5e0826f91abd7b2424fcb9acc3803cc9d61fab37
Kenza Boulisfane <35609559+bkenza@users.noreply.github.com>
2021-01-09 23:59:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import discord
 import os
 import requests
 import json
+from keep_alive import keep_alive
 
-client = discord.Client();
+client = discord.Client()
+
 
 def get_quote():
-  response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist&type=single")
-  json_data = json.loads(response.text)
-  joke = json_data["joke"]
-  return(joke)
+    response = requests.get(
+        "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist&type=single"
+    )
+    json_data = json.loads(response.text)
+    joke = json_data["joke"]
+    return (joke)
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -24,7 +30,9 @@ async def on_message(message):
         await message.channel.send('Hey there!')
 
     if message.content.startswith('$joke'):
-      quote = get_quote()
-      await message.channel.send(quote)
+        quote = get_quote()
+        await message.channel.send(quote)
 
+
+keep_alive()
 client.run(os.getenv('TOKEN'))
